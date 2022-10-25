2eb1d14c14dc47f65af3164107c5ac37ba307124
UwierzWSkilla <70282941+UwierzWSkilla@users.noreply.github.com>
2022-10-25 18:25:32 +0200

    something changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const { appendFile } = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app.get("/", function (req, res){
+  res.send("mixin' codeine");
+});
+
+app.listen(process.env.PORT || 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.addEventListener("hashchange", function(){
     const hash = window.location.hash.replace("#", "") || "home"
     const [page, article] = hash.split("/")
